c761beafa0bde286feca775ff05c51ce95849700
Green Apple <akshitverma7361@gmail.com>
2023-01-02 12:53:24 +0530

    updated modal ui structure

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import Modal from 'react-modal';
 import Image from "next/image";
 
+
+
 export default function Downloader({ChangeState,isModalOpen,data,CurrentID,preview,ChangePreview})
 {                   
     // assign the data so to be used later
@@ -47,7 +49,7 @@ export default function Downloader({ChangeState,isModalOpen,data,CurrentID,previ
         >
           <div className="main_Wrapper">
             <div className="view_wrapper">
-              <div className="left_view">
+              <div className="icon_view">
                 <div className="img_wrap">
                   <a
                     className="color"
@@ -98,55 +100,60 @@ export default function Downloader({ChangeState,isModalOpen,data,CurrentID,previ
                     />
                   </a>
                 </div>
-              </div>
-              <div className="right_view">
-                <div className="main_view">
-                  <Image
-                    id="current_img"
-                    alt={BlogData.title}
-                    width={400}
-                    height={400}
-                    src={preview}
-                    placeholder="empty"
-                    quality={100}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    priority={true}
-                    onLoad={SetButtonlnk}
-                  />
+                <div className="big_view">
+                  <div className="img_Wrapper">
+                    <Image
+                      id="current_img"
+                      alt={BlogData.title}
+                      width={400}
+                      height={400}
+                      src={preview}
+                      placeholder="empty"
+                      quality={100}
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      priority={true}
+                      onLoad={SetButtonlnk}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="btn_Wrap">
-                <div className="meta_wrapper">
+              <div className="editor_Wrap">
+                <div className="main_wrap">
                   <div className="logo_Wrap">
                     <img src="/3dicons.png" className="imglogo" />
                   </div>
-                  <div className="cpybtn_wrap">
-                    <a onClick={copyImg} className="cpy_btn">
-                      copy
-                    </a>
-                  </div>
-                  <div className="info_wrapper">
-                    <div className="meta">
-                      <h3>{BlogData.title}</h3>
-                      <p>
-                        <span>angle: clay</span> <br />
-                        <span>color: clay</span> <br />
-                        <span>size: clay</span> <br />
-                      </p>
+                  <div className="btns_wrap">
+                    <div className="cpybtn_wrap">
+                      <a onClick={copyImg} className="cpy_btn">
+                        copy
+                      </a>
                     </div>
-                  </div>
-                  <div className="angle_cntrl">
-                    <a className="iso">iso</a>
-                    <a className="daynmic">daynmic</a>
-                    <a className="front">front</a>
-                  </div>
-                  <div className="btns">
-                    <a id="download" className="btn download_btn">
-                      download
-                    </a>
-                    <a className="btn close" onClick={() => ChangeState(false)}>
-                      close
-                    </a>
+                    <div className="info_wrapper">
+                      <div className="meta">
+                        <h3>{BlogData.title}</h3>
+                        <p>
+                          <span>angle: clay</span> <br />
+                          <span>color: clay</span> <br />
+                          <span>size: clay</span> <br />
+                        </p>
+                      </div>
+                    </div>
+                    <div className="angle_cntrl">
+                      <a className="iso">iso</a>
+                      <a className="daynmic">daynmic</a>
+                      <a className="front">front</a>
+                    </div>
+                    <div className="btns">
+                      <a id="download" className="btn download_btn">
+                        download
+                      </a>
+                      <a
+                        className="btn close"
+                        onClick={() => ChangeState(false)}
+                      >
+                        close
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
